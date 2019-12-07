cf55d6a235cce7a1d43e4c95a8993e07fd1553f6
user <user01@KMS-MacBook.local>
2019-12-07 04:08:51 -0800

    more developments

---
+++ check.php
@@ -0,0 +1,17 @@
+<html>
+<body>
+
+<?php
+function input($type, $holder, $name){
+    	echo '<input type=$type placeholder=$holder value="" name=$name/>';
+
+}
+
+input("tel", "888-888-888", "telephone");
+
+?>
+</body>
+</html>
+	
+
+
--- index.php
+++ index.php
@@ -1,20 +1,24 @@
-<?php
-$title = "Your Custom Resume";
-
-function employment() {
-		
-	return <html><label>Phone Number</label></html>;
-	return <html><input type="tel" placeholder="888-888-8888" value="" name="telphone"/></html>;
-	
-}
-
-
-?>
-
 <html>
 	<head>
 		<link rel="stylesheet" href="resume.css"/>
+
 	<body>
+		<?php $title = "Your Custom Resume"; 
+		
+		  function input($type, $holder, $name){
+    	echo "<input type=$type placeholder=$holder value='' name=$name/>";
+ }
+			function label($label){
+			echo "<label>$label</label>";
+}
+			function message($message){
+			echo "<textarea rows='10'cols='50' placeholder=$message name='message'></textarea>";
+}	
+	
+		
+?>
+
+
 		<form action="/submit.php" method="POST">
 		<h1><?php echo $title; ?></h1>
 		<h3> Who are you? </h3>
@@ -29,13 +33,37 @@ function employment() {
 		<input type="tel" placeholder="888-888-8888" value="" name="telphone"/>
 		
 		<div class="a">
-		
-		<h3>Employment History<h3></div>
-		<label>Name of Employer</label>
-		<?php> employment(); ?>
+		<h3>Employment History<h3>
+		</div>
 
+
+
+<fieldset>
+<?php
 		
+		label("Employers Name");
+		input("text", "Employer", "employer");
+		label("Position");
+		input("text", "Position", "position");
 		
+		label("Location");
+		input("text", "City", "location");
+?> 
+<br>
+<?php		
+		message("Description of Job Duties");				
+?>
+<br>
+
+      <h4>Number of Years Worked</h4>			
+      <select name="years">
+        <option value ="1_5">1 year or less</option>
+        <option value ="2_5">2 to 5 years</option>
+        <option value="5_10">5 to 10 years</option>
+        <option value="10+">10 or more years</option>
+      </select>
+      <br />
+</fieldset>
 
 	</body>
 </html>
